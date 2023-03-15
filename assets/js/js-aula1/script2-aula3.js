25101461b6661440c03572230c77d11044e1a80f
LuizBedim <LuizOMBedim@gmail.com>
2023-03-14 21:26:24 -0300

    Organização de pastas e arquivos

---
+++ assets/js/js-aula1/script2-aula3.js
@@ -0,0 +1,40 @@
+
+function Carro() {
+    let marca;
+    let modelo;
+    let ano;
+    let cor;
+    let kilometragem;
+    let valor_fipe;
+
+    this.setAno = function(vAno) {
+        this.ano = vAno;
+    }
+    this.getAno = function() {
+        return this.ano;
+    }
+
+    this.setKilometragem = function(vKm) {
+        this.kilometragem = vKm;
+    }
+    this.getKilometragem = function() {
+        return this.kilometragem;
+    }
+
+    this.setValor = function(vValor) {
+        this.valor_fipe = vValor;
+    }
+    this.getValor = function() {
+        return this.valor_fipe;
+    }
+
+    
+
+}
+
+let c1 = new Carro();
+c1.setAno(2010);
+c1.teste();
+
+
+
